38e9146ff477c521e01f0b8e20317fe06b9513fe
HuzaifaKhanDeveloper <huzaifagmer@gmail.com>
2023-02-06 19:30:10 +0500

    first commit

---
+++ practical6.py
@@ -0,0 +1,9 @@
+#Question no 06
+o=input("Write Length of One Parallel Side:")
+v=float(o)
+m=input("Length Of Second Parallel Side :")
+i=float(m)
+l=input("Enter The Perpendilcular Distance:")
+z=float(l)
+k=z*(v+i)/2
+print("Area Of Trapezoid %.2f"%(k))# vigilant-octo-carnival
