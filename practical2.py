f757ee8531f7e077ea1d454dc5d12281a640acc5
HuzaifaKhanDeveloper <huzaifagmer@gmail.com>
2023-02-06 19:24:34 +0500

    first commit

---
+++ practical2.py
@@ -0,0 +1,9 @@
+#Question No 02
+print("Question No 02")
+a=input("Write Radius Of Circle:")
+r=float(a)
+import math
+c= math.pi*r**2
+d= 2*math.pi*r
+print("The Area And Circumference Of The Radius %.2f is %.2f and %.2f" % (r,c,d))# miniature-sniffle
+# vigilant-octo-carnival
