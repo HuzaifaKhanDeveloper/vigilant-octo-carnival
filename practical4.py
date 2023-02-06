0aaae51be1beba8ebec7c00281df3ecf081eab15
HuzaifaKhanDeveloper <huzaifagmer@gmail.com>
2023-02-06 19:28:28 +0500

    first commit

---
+++ practical4.py
@@ -0,0 +1,7 @@
+#QUESTION NO 04
+a=input("Enter Discounted Price:")
+m=float(a)
+b=input("Enter Discounted Percentage:")
+n=float(b)
+o=m*100/n
+print("Your Original Price is %f" %(o))# vigilant-octo-carnival
