b421225801548415f4fe00e1142eb412764a0950
HuzaifaKhanDeveloper <huzaifagmer@gmail.com>
2023-02-06 19:28:04 +0500

    first commit

---
+++ practical3.py
@@ -0,0 +1,6 @@
+a=input("Enter Selling Price:")
+m=float(a)
+b=input("Enter Discounted Percentage:")
+n=float(b)
+o=m-m*n/100
+print("Your Discounted Selling Price is %.2f" %(o))# vigilant-octo-carnival
