4ed5959a0841d5553fb805adfd3ca0a7d76892be
HuzaifaKhanDeveloper <huzaifagmer@gmail.com>
2023-02-06 19:24:03 +0500

    first commit

---
+++ practical1.py
@@ -0,0 +1,9 @@
+#QuestionNo01
+print("Question No 01")
+o=input("Enter Your Obtained Marks:")
+a=float(o)
+m=input("Enter Maximum Marks:")
+b=float(m)
+p=a/b*100
+print("Your Percentage Is %.2f"%(p))# refactored-carnival
+# vigilant-octo-carnival
